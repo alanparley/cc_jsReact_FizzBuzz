faf164d41b9facada13c69d0efaebaa3cec5ae99
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-12-08 14:14:01 +0000

    added fizzBuzz logic and showed results

--- src/containers/FizzBuzz.js
+++ src/containers/FizzBuzz.js
@@ -1,6 +1,53 @@
+import {useState, useEffect} from 'react';
+
+
 const FizzBuzz = () => {
+
+  const [currentNumber, setCurrentNumber] = useState(1);
+  const [resultNumber, setResultNumber] = useState ("");
+  const [showResult, setShowResult] = useState(false);
+
+  const handleInc = () => {
+      setCurrentNumber(currentNumber + 1);
+      setShowResult(false)
+
+  }
+  const handleClick = () => {
+    handleInc()
+  }
+
+  const handleShowResult = () => {
+    setShowResult(true);
+  }
+
+  const calcResult = () => {
+    if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
+      return "FizzBuzz";
+    } else if (currentNumber % 3 === 0) {
+      return "Fuzz";
+    } else if (currentNumber % 5 === 0) {
+      return "Buzz";
+    } else {
+      return currentNumber;
+    }
+  }
+
+  useEffect (()=>{
+    setResultNumber(calcResult());
+  }, [currentNumber])
+
   return (
-    <h1>Fizz Buzz Game!</h1>
+    <>
+      <h1>Fizz Buzz Game!</h1>
+      <h2> Number is {currentNumber} </h2>
+      { showResult ? <h2>Result is {resultNumber}</h2> : null}
+      <button onClick={handleShowResult}> Show Result</button>
+      <button onClick={handleClick}>Next Number</button>
+
+
+
+
+    </>
   )
 }
 
